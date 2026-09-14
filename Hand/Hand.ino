943f1734079373269986a5035e3eb6228d87dac4
RobJHill2 <robbiejameshill@gmail.com>
2026-09-14 08:56:48 +0100

    3rd finger added

--- Hand/Hand.ino
+++ Hand/Hand.ino
@@ -3,6 +3,8 @@
 #define EN_INDEX_IP 12
 #define EN_THUMB_MCP 11
 #define EN_THUMB_IP 10
+#define EN_MID_MCP 9
+#define EN_MID_IP 8
 
 #define O1_INDEX_MCP 22
 #define O2_INDEX_MCP 23
@@ -12,6 +14,10 @@
 #define O2_THUMB_MCP 27
 #define O1_THUMB_IP 28
 #define O2_THUMB_IP 29
+#define O1_MID_MCP 30
+#define O2_MID_MCP 31
+#define O1_MID_IP 32
+#define O2_MID_IP 33
 
 #define YSIG_INDEX_MCP 2 // interrupt pin
 #define BSIG_INDEX_MCP 53 // digital pin
@@ -21,9 +27,14 @@
 #define BSIG_THUMB_MCP 51
 #define YSIG_THUMB_IP 19
 #define BSIG_THUMB_IP 50
+#define YSIG_MID_MCP 20
+#define BSIG_MID_MCP 49
+#define YSIG_MID_IP 21
+#define BSIG_MID_IP 48
 
 #define INPUT_INDEX A1
-#define INPUT_THUMB A2
+#define INPUT_THUMB A3
+#define INPUT_MID A4
 
 #define MCP_MAX_ANGLE 90
 
@@ -50,26 +61,26 @@ class Joint {
     }
 
     double pid(int error) {
-      double now = millis();
-      double dt  = (now - last_time) / 1000.00;
+      now = millis();
+      dt  = (now - last_time) / 1000.00;
       last_time = now;
 
       if (abs(error) <= TOLERANCE) error = 0;
-      double proportional = error;
+      proportional = error;
       integral += error * dt;
-      double differential = (error - prev_error) / dt;
+      differential = (error - prev_error) / dt;
       prev_error = error;
 
       return (Kp * proportional) + (Ki * integral) + (Kd * differential);
     }
 
-    void motorSpeed(int speed) {
-      if (speed > 100) speed = 100;
-      if (speed < -100) speed = -100;
+    void motorSpeed(int velocity) {
+      if (velocity > 100) velocity = 100;
+      if (velocity < -100) velocity = -100;
 
-      analogWrite(EN, int(abs(speed)*2.55));
+      analogWrite(EN, int(abs(velocity)*2.55));
 
-      if (speed > 0) {
+      if (velocity > 0) {
         digitalWrite(O1, HIGH); // forwards
         digitalWrite(O2, LOW);
       } else {
@@ -93,6 +104,11 @@ class Joint {
     double integral = 0;
     double prev_error = 0;
     double last_time = 0;
+    double now;
+    double dt;
+    double proportional;
+    double differential;
+    
     uint8_t EN;
     uint8_t O1;
     uint8_t O2;
@@ -114,14 +130,16 @@ class Finger {
     }
 
     tick() {
-      int potVal = analogRead(IN);
+      potVal = analogRead(IN);
       
-      int setpoint = (potVal - initPotVal) / 5;
+      setpoint = (potVal - initPotVal) / 5;
       setpoint = round((float)setpoint / 10)*10;
       // setpoint measured in degrees from starting position
 
-      int IPsetpoint;
-      int MCPsetpoint;
+      if (setpoint > 120) {
+        setpoint = 120;
+      }
+
       if (setpoint > MCP_MAX_ANGLE) {
         IPsetpoint = setpoint - MCP_MAX_ANGLE;
         MCPsetpoint = MCP_MAX_ANGLE;
@@ -133,8 +151,6 @@ class Finger {
         MCPsetpoint = setpoint;
       }
 
-      double pidOutput;
-
       pidOutput = IP->pid(IPsetpoint - IP->angle);
       IP->motorSpeed((int)(pidOutput * SPEEDFACTOR));
 
@@ -145,10 +161,16 @@ class Finger {
   private:
     uint8_t IN;
     uint8_t initPotVal;
+    int potVal;
+    int setpoint;
+    int IPsetpoint;
+    int MCPsetpoint;
+    double pidOutput;
 };
 
 Finger* index;
 Finger* thumb;
+Finger* middle;
 
 void setup() {
   Serial.begin(115200);
@@ -184,8 +206,24 @@ void setup() {
   
   pinMode(INPUT_THUMB, INPUT);
 
+  pinMode(EN_MID_IP, OUTPUT);
+  pinMode(EN_MID_MCP, OUTPUT);
+
+  pinMode(O1_MID_IP, OUTPUT);
+  pinMode(O2_MID_IP, OUTPUT);
+  pinMode(O1_MID_MCP, OUTPUT);
+  pinMode(O2_MID_MCP, OUTPUT);
+
+  pinMode(YSIG_MID_IP, INPUT);
+  pinMode(BSIG_MID_IP, INPUT);  
+  pinMode(YSIG_MID_MCP, INPUT);
+  pinMode(BSIG_MID_MCP, INPUT);
+  
+  pinMode(INPUT_MID, INPUT);
+
   index = new Finger(INPUT_INDEX, EN_INDEX_IP, O1_INDEX_IP, O2_INDEX_IP, YSIG_INDEX_IP, BSIG_INDEX_IP, EN_INDEX_MCP, O1_INDEX_MCP, O2_INDEX_MCP, YSIG_INDEX_MCP, BSIG_INDEX_MCP);
   thumb = new Finger(INPUT_THUMB, EN_THUMB_IP, O1_THUMB_IP, O2_THUMB_IP, YSIG_THUMB_IP, BSIG_THUMB_IP, EN_THUMB_MCP, O1_THUMB_MCP, O2_THUMB_MCP, YSIG_THUMB_MCP, BSIG_THUMB_MCP);
+  middle = new Finger(INPUT_MID, EN_MID_IP, O1_MID_IP, O2_MID_IP, YSIG_MID_IP, BSIG_MID_IP, EN_MID_MCP, O1_MID_MCP, O2_MID_MCP, YSIG_MID_MCP, BSIG_MID_MCP);
 
   attachInterrupt(digitalPinToInterrupt(YSIG_INDEX_MCP), index_MCP_ISR, CHANGE);
   attachInterrupt(digitalPinToInterrupt(YSIG_INDEX_IP), index_IP_ISR, CHANGE);
@@ -193,12 +231,16 @@ void setup() {
   attachInterrupt(digitalPinToInterrupt(YSIG_THUMB_MCP), thumb_MCP_ISR, CHANGE);
   attachInterrupt(digitalPinToInterrupt(YSIG_THUMB_IP), thumb_IP_ISR, CHANGE);
 
+  attachInterrupt(digitalPinToInterrupt(YSIG_MID_MCP), middle_MCP_ISR, CHANGE);
+  attachInterrupt(digitalPinToInterrupt(YSIG_MID_IP), middle_IP_ISR, CHANGE);
+
   Serial.println("hello world");
 }
 
 void loop() {
   index->tick();
   thumb->tick();
+  middle->tick();
 
   delay(10);
 }
@@ -217,4 +259,12 @@ void thumb_MCP_ISR() {
 
 void thumb_IP_ISR() {
   thumb->IP->countDegrees();
+}
+
+void middle_MCP_ISR() {
+  middle->MCP->countDegrees();
+}
+
+void middle_IP_ISR() {
+  middle->IP->countDegrees();
 }
